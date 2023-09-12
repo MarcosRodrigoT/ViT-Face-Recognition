28461ac881956be8f48aa8d2f2de7c3eb763d59c
mrt <marcos.rodrigo@upm.es>
2023-09-12 14:18:18 +0200

    Add funtion to get unique names

--- real_world_occluded_faces_test.py
+++ real_world_occluded_faces_test.py
@@ -2,8 +2,8 @@ import os
 
 
 def remove_empty_directories():
-    masked_items = os.listdir(masked_dir)
     neutral_items = os.listdir(neutral_dir)
+    masked_items = os.listdir(masked_dir)
     sunglasses_items = os.listdir(sunglasses_dir)
 
     for item in masked_items:
@@ -19,9 +19,20 @@ def remove_empty_directories():
             print(f'Sunglasses folder ->\t{item} has no images, removing directory')
             os.rmdir(f'{sunglasses_dir}/{item}')
 
+def get_unique_names():
+    neutral_items = os.listdir(neutral_dir)
+    masked_items = list(map(lambda x: x.split('_wearing_mask')[0], os.listdir(masked_dir)))
+    sunglasses_items = list(map(lambda x: x.split('_wearing_sunglasses')[0], os.listdir(sunglasses_dir)))
+    return sorted(
+        set(masked_items + neutral_items + sunglasses_items)
+    )
+
 
 base_dir = '/mnt/Data/mrt/RealWorldOccludedFaces/images'
 masked_dir = f"{base_dir}/masked"
 neutral_dir = f"{base_dir}/neutral"
 sunglasses_dir = f"{base_dir}/sunglasses"
+
 remove_empty_directories()
+
+unique_names = get_unique_names()
