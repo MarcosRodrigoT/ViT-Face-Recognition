d76270fd1c6b963801489f93fd846ca61770fe8b
mrt <marcos.rodrigo@upm.es>
2023-09-12 14:43:20 +0200

    Get images histogram

--- real_world_occluded_faces_test.py
+++ real_world_occluded_faces_test.py
@@ -7,14 +7,14 @@ def remove_empty_directories():
     masked_items = os.listdir(MASKED_DIR)
     sunglasses_items = os.listdir(SUNGLASSES_DIR)
 
-    for item in masked_items:
-        if not os.listdir(f"{MASKED_DIR}/{item}"):
-            print(f'Masked folder ->\t{item} has no images, removing directory')
-            os.rmdir(f'{MASKED_DIR}/{item}')
     for item in neutral_items:
         if not os.listdir(f"{NEUTRAL_DIR}/{item}"):
             print(f'Neutral folder ->\t{item} has no images, removing directory')
             os.rmdir(f'{NEUTRAL_DIR}/{item}')
+    for item in masked_items:
+        if not os.listdir(f"{MASKED_DIR}/{item}"):
+            print(f'Masked folder ->\t{item} has no images, removing directory')
+            os.rmdir(f'{MASKED_DIR}/{item}')
     for item in sunglasses_items:
         if not os.listdir(f"{SUNGLASSES_DIR}/{item}"):
             print(f'Sunglasses folder ->\t{item} has no images, removing directory')
@@ -31,11 +31,32 @@ def get_unique_names():
     return sorted(set(masked_items + neutral_items + sunglasses_items))
 
 
+def get_histogram():
+    data = {key: {'Neutral': None, 'Masked': None, 'Sunglasses': None} for key in UNIQUE_NAMES}
+
+    neutral_items = os.listdir(NEUTRAL_DIR)
+    masked_items = os.listdir(MASKED_DIR)
+    sunglasses_items = os.listdir(SUNGLASSES_DIR)
+
+    for item in neutral_items:
+        name = item
+        data[name]['Neutral'] = len(os.listdir(f"{NEUTRAL_DIR}/{item}"))
+    for item in masked_items:
+        name = item.split('_wearing_mask')[0]
+        data[name]['Masked'] = len(os.listdir(f"{MASKED_DIR}/{item}"))
+    for item in sunglasses_items:
+        name = item.split('_wearing_sunglasses')[0]
+        data[name]['Sunglasses'] = len(os.listdir(f"{SUNGLASSES_DIR}/{item}"))
+    return data
+
+
 BASE_DIR = '/mnt/Data/mrt/RealWorldOccludedFaces/images'
-MASKED_DIR = f"{BASE_DIR}/masked"
 NEUTRAL_DIR = f"{BASE_DIR}/neutral"
+MASKED_DIR = f"{BASE_DIR}/masked"
 SUNGLASSES_DIR = f"{BASE_DIR}/sunglasses"
 
 remove_empty_directories()
 
-unique_names = get_unique_names()
+UNIQUE_NAMES = get_unique_names()
+
+DATA_HISTOGRAM = get_histogram()
