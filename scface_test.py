7d2ccbd4fdec3f617392dc51492a6686c1b2bc10
mrt <marcos.rodrigo@upm.es>
2023-09-13 12:57:54 +0200

    Add mugshot and surveillance directories

--- scface_test.py
+++ scface_test.py
@@ -1,3 +1,7 @@
 import os
 
 BASE_DIR = '/mnt/Data/mrt/SCface_database'
+MUGSHOT_DIR = f'{BASE_DIR}/mugshot_frontal_cropped_all'
+SURVEILLANCE_DIR = f'{BASE_DIR}/surveillance_cameras_all'
+
+print(os.listdir(SURVEILLANCE_DIR))
