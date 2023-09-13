c54c298bdf0f6edab8085f6e6b7c1db1a070e9d4
mrt <marcos.rodrigo@upm.es>
2023-09-13 13:00:35 +0200

    Add small change

--- scface_test.py
+++ scface_test.py
@@ -4,4 +4,4 @@ BASE_DIR = '/mnt/Data/mrt/SCface_database'
 MUGSHOT_DIR = f'{BASE_DIR}/mugshot_frontal_cropped_all'
 SURVEILLANCE_DIR = f'{BASE_DIR}/surveillance_cameras_all'
 
-print(os.listdir(SURVEILLANCE_DIR))
+print(os.listdir(MUGSHOT_DIR))
