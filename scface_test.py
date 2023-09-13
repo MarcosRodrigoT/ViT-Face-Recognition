5b8d6e2b304bdbecadb470ed84915a57b8edc4ad
mrt <marcos.rodrigo@upm.es>
2023-09-13 15:46:15 +0200

    Load data

--- scface_test.py
+++ scface_test.py
@@ -1,7 +1,26 @@
 import os
+import tensorflow as tf
+from vit_keras import vit
 
+
+"""
+CREATE DATASET
+"""
 BASE_DIR = '/mnt/Data/mrt/SCface_database'
 MUGSHOT_DIR = f'{BASE_DIR}/mugshot_frontal_cropped_all'
 SURVEILLANCE_DIR = f'{BASE_DIR}/surveillance_cameras_all'
 
-print(os.listdir(MUGSHOT_DIR))
+mugshot_data = {}
+for file in os.listdir(MUGSHOT_DIR):
+    person = file.split('_')[0]
+    file_path = os.path.join(MUGSHOT_DIR, file)
+    mugshot_data[person] = {'file': file_path, 'embeddings': None}
+
+surveillance_data = {}
+for person in mugshot_data.keys():
+    surveillance_data[person] = {'files': [], 'embeddings': []}
+for file in os.listdir(SURVEILLANCE_DIR):
+    person = file.split('_')[0]
+    file_path = os.path.join(SURVEILLANCE_DIR, file)
+    surveillance_data[person]['files'].append(file_path)
+    surveillance_data[person]['embeddings'].append(None)
